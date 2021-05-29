a7e86c1904e1a6de5e16d0215d2813dabc37749b
Allyson1602 <aathyrson@yahoo.com.br>
2021-05-29 15:41:16 -0300

    Projeto inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from 'src/pages/main/main-page.component';
+import { RegisterComponent } from 'src/pages/register/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MainComponent },
+  { path: "register", component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
